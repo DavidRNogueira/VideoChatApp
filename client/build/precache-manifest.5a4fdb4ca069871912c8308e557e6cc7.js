7a3007dfbda0fc5b730153f3ae8f04f73a2b9543
davidrnogueira <davidrnogueira@hotmail.com>
2020-05-04 18:10:43 -0700

    Build

---
+++ client/build/precache-manifest.5a4fdb4ca069871912c8308e557e6cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb44079fc00b1bb86ae77645d37123c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46a90755db8393f216e",
+    "url": "/static/css/main.6888bee2.chunk.css"
+  },
+  {
+    "revision": "bf747d76e72340310403",
+    "url": "/static/js/2.c13768db.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.c13768db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46a90755db8393f216e",
+    "url": "/static/js/main.0183ad77.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
